ff77d909ce2dae08945895cc0d917aad677dea74
Keval Shah <145671282+Kevalshah91@users.noreply.github.com>
2024-04-19 11:48:47 +0530

    Add files via upload

--- mongoose_express_basics/app.js
+++ mongoose_express_basics/app.js
@@ -1,68 +1,35 @@
-
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const tutorialRoutes = require('./routes/tutorial.routes');
+
 const app = express();
-app.use(express.json());
-const users = [
-  { id: 1, name: 'Keval Shah' },
-  { id: 2, name: 'Virat Kolhi' },
-];
+const PORT = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mango', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+// Test route
 app.get('/', (req, res) => {
-  res.send('Hello, this is the root endpoint!');
-});
-app.get('/users', (req, res) => {
-  res.json(users);
+  res.send('Welcome to the Express-Mongo CRUD API');
 });
 
-// Get a specific user by ID
-app.get('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const user = users.find(u => u.id === userId);
+// Include routes
+app.use('/tutorials', tutorialRoutes);
 
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
-});
-
-// Add a new user
-app.post('/users', (req, res) => {
-  const newUser = req.body;
-  users.push(newUser);
-  res.status(201).json(newUser);
-});
-
-// Update a user by ID
-app.put('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const updatedUser = req.body;
-  const index = users.findIndex(u => u.id === userId);
-
-  if (index !== -1) {
-    users[index] = { ...users[index], ...updatedUser };
-    res.json(users[index]);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
-});
-
-// Delete a user by ID
-app.delete('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const index = users.findIndex(u => u.id === userId);
-
-  if (index !== -1) {
-    const deletedUser = users.splice(index, 1)[0];
-    res.json(deletedUser);
-  } else {
-    res.status(404).json({ message: 'User not found' });
-  }
-});
-
-// Set the port for the server to listen on
-const port = 3000;
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
